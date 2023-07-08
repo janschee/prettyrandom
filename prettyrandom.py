0551b74019c42807b7cda0fa07f1fc23541ad79c
janschee <jan.scheerer@icloud.com>
2023-07-08 19:07:13 +0000

    add function for specifying the character set

--- prettyrandom.py
+++ prettyrandom.py
@@ -1,5 +1,5 @@
 #%%
-from typing import Any, List, Dict, Callable
+from typing import List, Callable
 import random
 import unittest
 
@@ -9,8 +9,19 @@ class PrettyRandom():
     def __init__(self) -> None:
         self.rules: List[Callable] = [self.repeat, self.alternate, self.pairs, self.outlier, self.zerofill]
         self.numbers : List[str] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-        self.letters : List[str] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-        self.capitals : List[str] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        self.lowercase : List[str] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        self.uppercase : List[str] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+        # Default behaviour: Only use numbers and uppercase characters
+        self.character_set: List[str] = []
+        self.config(use_numbers=True, use_lowercase=False, use_uppercase=True)
+
+    def config(self, use_numbers: bool, use_lowercase: bool, use_uppercase: bool) -> None:
+        assert (use_numbers or use_lowercase or use_uppercase) == True, "At least one of the options has to be set to True."
+        self.character_set = []
+        if use_numbers: self.character_set += self.numbers
+        if use_lowercase: self.character_set += self.lowercase
+        if use_uppercase: self.character_set += self.uppercase
 
     def repeat(self, char1: str, char2: str, blocksize: int) -> str:
         char: str = random.choice([char1, char2])
@@ -43,29 +54,25 @@ class PrettyRandom():
     
     def random_rule(self) -> Callable:
         return random.choice(self.rules)
-    
-    def random_char(self, numbers: bool, letters: bool, capitals: bool) -> str:
-        pool: List[str] =[]
-        if numbers: pool += self.numbers
-        if letters: pool += self.letters
-        if capitals: pool += self.capitals
-        return random.choice(pool)
 
     def __call__(self, blocksize: int, length: int) -> str:
         assert length >= blocksize, f"Length ({length}) must be larger or equal than the block size ({blocksize})!"
         num_blocks = length // blocksize
         rest = length % blocksize
         res = ""
+
+        # Generate blocks
         for _ in range(num_blocks):
-            randchar1 = self.random_char(numbers=True, letters=False, capitals=True)
-            randchar2 = self.random_char(numbers=True, letters=False, capitals=True)
+            randchar1 = random.choice(self.character_set)
+            randchar2 = random.choice(self.character_set)
             rule = self.random_rule()
             res += rule(randchar1, randchar2, blocksize)
             res += " "
 
+        # Fill up remaining characters with alternate patterns
         if rest != 0:
-            randchar1 = self.random_char(numbers=True, letters=False, capitals=True)
-            randchar2 = self.random_char(numbers=True, letters=False, capitals=True)
+            randchar1 = random.choice(self.character_set)
+            randchar2 = random.choice(self.character_set)
             res += self.alternate(randchar1, randchar2, rest)
         return str(res)
         
@@ -87,13 +94,13 @@ class Test(unittest.TestCase):
                 x = x.strip() #remove whitespaces
                 blocks: List[str] = x.split(" ")
                 if len(blocks) > 1 and len(blocks[-1]) != len(blocks[-2]): blocks = blocks[:-1]
-                print(blocks)
                 for b in blocks:
                     self.assertEqual(len(b), blocksize)
 
 
 if __name__ == "__main__":
     prettyrandom = PrettyRandom()
+    prettyrandom.config(True, True, True)
     blocksize = 4
     length = 22
     print(prettyrandom(blocksize, length))
